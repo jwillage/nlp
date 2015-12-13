342b0213de386fcd1224aaea080498c4fde173ff
jwillage <jwillage@gmail.com>
2015-12-13 17:34:15 -0500

    Switching over from tm to quanteda

--- start.R
+++ start.R
@@ -7,28 +7,36 @@ download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Courser
 l <- unzip(fn, list = T)
 
 # #work with text documents in list format
-# tmp <- list()
-# for (i in l$Name[grep("US.*txt", l$Name)])
-#   tmp[[length(tmp) + 1]] <- readLines(unz("data/Coursera-SwiftKey.zip", i))
-# tmp <- setNames(tmp, gsub(".txt", "" ,gsub(".*/.*/", "", l$Name[grep("txt", l$Name)])))
+tmp <- list()
+#for (i in l$Name[grep("US.*txt", l$Name)])
+for (i in l$Name)
+  tmp[[length(tmp) + 1]] <- readLines(unz("data/Coursera-SwiftKey.zip", i))
+tmp <- setNames(tmp, gsub(".txt", "" ,gsub(".*/.*/", "", l$Name[grep("txt", l$Name)])))
+saveRDS(tmp, "data/readLines_list_allFiles.RDS")
 
-#use dbConrol parm to create a database on disk instead of storing everything in memory
+en <- list()
+en[["twitter"]] <- tmp[[7]]; en[["news"]] <- tmp[[8]]; en[["blogs"]] <- tmp[[9]];
+rm(tmp)
+saveRDS(en, "data/readLines_list_all_EN.RDS")
 
-# #some functions available with tm
-# stemDocument(c[[1]])$content
-# inspect(crudeTDM)[crudeTDMhf, 1:10]
-# findAssocs(crudeTDM, "saudi", .8)
+nc <- sapply(en, nchar)
+lapply(nc, max)
+#the longest document is in the blogs data set, over 40K characters
 
-zs <- ZipSource(fn, pattern = "US.*txt", recursive = T)
-corp <- Corpus(zs, readerControl = list(reader = readPlain,  language = "en_US", load = TRUE))
+# #read each file in as 1 large corpus
+# zs <- ZipSource(fn, pattern = "US.*txt", recursive = T)
+# corp <- Corpus(zs, readerControl = list(reader = readPlain,  language = "en_US", load = TRUE))
 
-corp.samp <- list()
-for (i in l$Name[grep("US.*txt", l$Name)]){
-  df.source <- DataframeSource(data.frame(c= readLines(unz("data/Coursera-SwiftKey.zip", i), 1000)))
-  corp.samp[[length(corp.samp) + 1]] <- Corpus(df.source, readerControl = 
-                                                 list(reader = readPlain,  language = "en_US", 
-                                                      load = TRUE))
-}
+# #read in files broken up by line
+# corp.samp <- list()
+# for (i in l$Name[grep("US.*txt", l$Name)]){
+#   df.source <- DataframeSource(data.frame(c= readLines(unz(fn, i), 1000)))
+#   corp.samp[[length(corp.samp) + 1]] <- Corpus(df.source, readerControl = 
+#                                                  list(reader = readPlain,  language = "en_US", 
+#                                                       load = TRUE))
+# }
+
+corp <- lapply(en, corpus)
 
 #when ready, combine into single corpus
 #t <- c(corp.samp[[1]], corp.samp[[2]], corp.samp[[3]])
@@ -39,6 +47,16 @@ rw <- function(x) removeWords(x, sw)
 #we make the decision to remove punctuation, numbers, whitepsace, stopwords, change to lower
 funs <- list(stripWhitespace, rw, content_transformer(tolower), removePunctuation, removeNumbers)
 corp.samp.mod <- lapply(corp.samp, function(x) tm_map(x, FUN = tm_reduce, tmFuns = funs))
+
+
+corp.twitter <- corpus(t[[1]])
+dfm.en <- lapply(t, dfm)
+
+dfm.twitter.tm <- as.TermDocumentMatrix(dfm.twitter, weighting = weightTf)
+
+#think about removing terms that only appear once?
+#try removeSparseTerms(bigTDM, sparse= 0.8)
+
 
 #tokenize
 #MC_tokenize each document in a corpus. lapply that over each of the corpora. 
@@ -62,7 +80,7 @@ cbind(freq.sort[[1]][1:50,], freq.sort[[2]][1:50,], freq.sort[[3]][1:50,])
 
 #we see the top 50 tokens for twitter, news, blogs, respectively
 #twitter seems to be a lot more diversified, with only two terms from the sample appearing > 100x
-#compare how conversation twitter is, with most freq term being "you" (also see "your" at no. 3)
+#compare how conversational twitter is, with most freq term being "you" (also see "your" at no. 3)
 #that appears much lower in news, and blogs somehwere in between. 
 
-
+removeFeatures(x, stopwords())
