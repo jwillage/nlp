4d959547254006b4d9e4634347f64baf2a59963f
jwillage <jwillage@gmail.com>
2015-12-16 12:09:36 -0500

    first run at bigram model

--- start.R
+++ start.R
@@ -66,8 +66,7 @@ bigram <- lapply(corp, function(x) tokenize(toLower(x), removeNumbers = T, remov
 trigram <- lapply(corp, function(x) tokenize(toLower(x), removeNumbers = T, removePunct = T, 
                                             removeSeparators = T, ngrams = 3))
 
-#explore n-grams in each corpus
-
+#explore n-grams in each corpus:
 #Total terms
 
 #unique unigrams
@@ -75,25 +74,32 @@ sapply(dfm.en, ncol)
 #twitter    news   blogs 
 #419417  331755  380867 
 #unique bigrams
-
+sapply(bigram, function(x) length(unique(unlist(x))))
+#twitter    news   blogs 
+#5281764 6296250 6341521 
 #unique trigrams
 sapply(trigram, function(x) length(unique(unlist(x))))
 # twitter     news    blogs 
 #13581787 17875031 18951463 
 
-#leave in profranity and manually give them a 0 probability. This way we don't make weird n-grams
-#of incomplete sentences. 
-#consider leaving in numbers for the same reason
+#consider leaving in numbers so we dont have incomplete ngrams. or filter out at the gram stage
 #handle twitter mentions. otherwise replace @ with "at"
+#twitter hashtag removal. just remove octothorpe if it at the end of a tweet, otherwise,  gaps
+#break sentences on punctuation
+#recreate grams with space instead of underscore - easier to match on later
 
 #calculate frequencies
-bi.samp <- bigram[[1]][1:10000]
-bi.samp.un <- unlist(bi.samp)
-freq <- table(bi.samp.un)/length(bi.samp.un)
-tf <- as.data.frame(freq, row.names = NULL, stringsAsFactors = F)
-names(tf) <- c("term", "rf")
+bi.samp <- lapply(bigram, function(x) x[1:10000])
+bi.samp.un <- lapply(bi.samp, unlist)
+#rounding to 8 worksk for sample, may need to increase precision for larger corpora
+#ie length of samp[[1]] == 114293. 1/114293 = 8.7e-6. can round after that (keep 2 extra JICOC)
+freq <- lapply(bi.samp.un, function(x) round(table(x)/length(x), 8))
+tf <- lapply(freq, function(x) as.data.frame(x, row.names = NULL, stringsAsFactors = F))
+rm(freq)
 #here we see the top relative frequencies
-head(tf[order(tf$rf, decreasing = T),], 50)
+lapply(tf, function(x) head(x[order(x$Freq, decreasing = T),], 50))
+#while the most popular tokens were different, the most popular grams are similar across each of the
+#sources - the grams that help build sentences, "of the", "in the", "to the"
 
 #remove grams with profanity
 sw <- paste0("(([^a-z]+|^)(", 
@@ -101,6 +107,22 @@ sw <- paste0("(([^a-z]+|^)(",
                              "List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/raw/master/en")
                              ), sep = "", collapse = "|")
              , ")([^a-z]+|$)|", paste0(readLines("stop.txt"), sep = "", collapse = "|"), ")")
-#further filter profane words that may appear mid-word
-idx <- grep(sw, tf$term)  #remove these
-tf <- tf[-idx, ]
+tf <- lapply(tf, function(x) x[-grep(sw, x$x), ])
+rm(sw)
+
+#Indexing. For bigrams, only take [1] the first word to index
+for (i in 1:length(tf)) tf[[i]]$idx <-  unlist(lapply(strsplit(tf[[i]]$x, "_"), function(x) x[1]))
+
+#only keep the most frequen ngram by index
+#we can go back and find term(s) matching this max freq and idx
+lookup.bi <- lapply(tf, function(x) setNames(aggregate(x$Freq, by = list(x$idx), max), c("idx", "Freq")))
+
+model.bi <- list()
+for (i in 1:length(tf)) model.bi[[i]] <- merge(lookup.bi[[i]], tf[[i]])
+#remove duplicate idx and freq, taking the first (alpha) occurance. ok to drop freq at this point
+model.bi <- lapply(model.bi, function(x) x[!duplicated(x$idx), c("idx", "x")])
+#sanity check, "i love" highest freq for index "i" in twitter
+#tf[[1]][tf[[1]]$idx == "i", ][order(tf[[1]][tf[[1]]$idx == "i", "Freq"]), ]
+#model.bi[[1]][model.bi[[1]]$idx == "i",]
+rm(bi.samp.un); rm(tf); rm(lookup.bi)
+
