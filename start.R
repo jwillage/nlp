ca2f1179fed1604aba46d15ff67a15acaf2bd372
jwillage <jwillage@gmail.com>
2015-12-16 17:23:17 -0500

    functionalized n-gram model creation

--- start.R
+++ start.R
@@ -89,40 +89,64 @@ sapply(trigram, function(x) length(unique(unlist(x))))
 #recreate grams with space instead of underscore - easier to match on later
 
 #calculate frequencies
+createModel <- function(gram, n){
+  #assumes list of corpora
+  
+  gram <- lapply(gram, unlist)
+  #rounding to 8 works for sample, may need to increase precision for larger corpora
+  #ie length of samp[[1]] == 114293. 1/114293 = 8.7e-6. can round after that (keep 2 extra JICOC)
+  
+  freq <- lapply(gram, function(x) round(table(x)/length(x), 8))
+  tf <- lapply(freq, function(x) as.data.frame(x, row.names = NULL, stringsAsFactors = F))
+  rm(freq)
+  #here we see the top relative frequencies
+  #lapply(tf, function(x) head(x[order(x$Freq, decreasing = T),], 50))
+  #while the most popular tokens were different, the most popular grams are similar across each of the
+  #sources - the grams that help build sentences, "of the", "in the", "to the"
+  
+  #remove grams with profanity
+  sw <- paste0("(([^a-z]+|^)(", 
+               paste(readLines(paste0("https://github.com/shutterstock/",
+                               "List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/raw/master/en")
+                               ), sep = "", collapse = "|")
+               , ")([^a-z]+|$)|", paste0(readLines("stop.txt"), sep = "", collapse = "|"), ")")
+  tf <- lapply(tf, function(x) x[-grep(sw, x$x), ])
+  rm(sw)
+  
+  #Indexing. For bigrams, only take [1] the first word to index
+  #for n>2, need to combine 1 - n-1 into a single word index
+  for (i in 1:length(tf)){
+    t <- unlist(lapply(strsplit(tf[[i]]$x, "_"), function(x) x[1:n-1]))
+    p <- t[seq(1, length(t), by = n - 1)]
+    #process additional grams if n > 2
+    for(j in 2:(n - 1)) p <- paste(p, t[seq(j, length(t), by = n - 1)])
+    tf[[i]]$idx <- p
+  }
+  
+  #only keep the most frequent ngram by index
+  #we can go back and find term(s) matching this max freq and idx
+  lookup <- lapply(tf, function(x) setNames(aggregate(x$Freq, by = list(x$idx), max), 
+                                            c("idx", "Freq")))
+  
+  model <- list()
+  for (i in 1:length(tf)) model[[i]] <- merge(lookup[[i]], tf[[i]])
+  #remove duplicate idx and freq, taking the first (alpha) occurance. ok to drop freq at this point
+  model <- lapply(model, function(x) x[!duplicated(x$idx), c("idx", "x")])
+  #sanity check, "i love" highest freq for index "i" in twitter
+  #tf[[1]][tf[[1]]$idx == "i", ][order(tf[[1]][tf[[1]]$idx == "i", "Freq"]), ]
+  #model.bi[[1]][model.bi[[1]]$idx == "i",]
+  rm(gram); rm(tf); rm(lookup)
+  #need to combine back into 1. need to figure out how to deal with mismatching index/freq combos
+  model
+}
+
+#Index n-grams
+bigram <- readRDS("data/bigram_EN.RDS")
 bi.samp <- lapply(bigram, function(x) x[1:10000])
-bi.samp.un <- lapply(bi.samp, unlist)
-#rounding to 8 worksk for sample, may need to increase precision for larger corpora
-#ie length of samp[[1]] == 114293. 1/114293 = 8.7e-6. can round after that (keep 2 extra JICOC)
-freq <- lapply(bi.samp.un, function(x) round(table(x)/length(x), 8))
-tf <- lapply(freq, function(x) as.data.frame(x, row.names = NULL, stringsAsFactors = F))
-rm(freq)
-#here we see the top relative frequencies
-lapply(tf, function(x) head(x[order(x$Freq, decreasing = T),], 50))
-#while the most popular tokens were different, the most popular grams are similar across each of the
-#sources - the grams that help build sentences, "of the", "in the", "to the"
-
-#remove grams with profanity
-sw <- paste0("(([^a-z]+|^)(", 
-             paste(readLines(paste0("https://github.com/shutterstock/",
-                             "List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/raw/master/en")
-                             ), sep = "", collapse = "|")
-             , ")([^a-z]+|$)|", paste0(readLines("stop.txt"), sep = "", collapse = "|"), ")")
-tf <- lapply(tf, function(x) x[-grep(sw, x$x), ])
-rm(sw)
-
-#Indexing. For bigrams, only take [1] the first word to index
-for (i in 1:length(tf)) tf[[i]]$idx <-  unlist(lapply(strsplit(tf[[i]]$x, "_"), function(x) x[1]))
-
-#only keep the most frequen ngram by index
-#we can go back and find term(s) matching this max freq and idx
-lookup.bi <- lapply(tf, function(x) setNames(aggregate(x$Freq, by = list(x$idx), max), c("idx", "Freq")))
-
-model.bi <- list()
-for (i in 1:length(tf)) model.bi[[i]] <- merge(lookup.bi[[i]], tf[[i]])
-#remove duplicate idx and freq, taking the first (alpha) occurance. ok to drop freq at this point
-model.bi <- lapply(model.bi, function(x) x[!duplicated(x$idx), c("idx", "x")])
-#sanity check, "i love" highest freq for index "i" in twitter
-#tf[[1]][tf[[1]]$idx == "i", ][order(tf[[1]][tf[[1]]$idx == "i", "Freq"]), ]
-#model.bi[[1]][model.bi[[1]]$idx == "i",]
-rm(bi.samp.un); rm(tf); rm(lookup.bi)
+rm(bigram)
+trigram <- readRDS("data/trigram_EN.RDS")
+tri.samp <- lapply(trigram, function(x) x[1:10000])
+rm(trigram)
+model.bi <- createModel(bi.samp)
+model.tri <- createModel(tri.samp)
 
