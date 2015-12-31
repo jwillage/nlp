d2d7373868dd70e0db7df9bfa4a139de47ac571a
jwillage <jwillage@gmail.com>
2015-12-31 17:30:42 -0500

    update shiny with working models

---
+++ shiny/nlprediction/predict.R
@@ -0,0 +1,25 @@
+bigram.model.samp <- readRDS("model_bi_sample_100k_EN_new.RDS")
+trigram.model.samp <- readRDS("model_tri_sample_100k_EN_new.RDS")
+quad.model.samp <- readRDS("model_quad_sample_100k_EN_new.RDS")
+
+stupidBackoff <- function(phrase){
+  phrase <- strsplit(phrase, " ")[[1]]
+  len <- length(phrase)
+  bi <- NULL; tri <- NULL; quad <- NULL;
+  if (len >= 1) 
+    bi <- bigram.model.samp[bigram.model.samp$idx == 
+                              paste(phrase[len], collapse = " "), ]
+  if (len >= 2)
+    tri <- trigram.model.samp[trigram.model.samp$idx == 
+                                paste(phrase[(len - 1) : len], collapse = " "), ]
+  if (len >= 3)
+    quad <- quad.model.samp[quad.model.samp$idx == 
+                              paste(phrase[(len - 2) : len], collapse = " "), ]
+
+  ifelse (length(quad$gram) > 0, quad$gram, 
+          ifelse (length(tri$gram) > 0, tri$gram, 
+                  ifelse (length(bi$gram) > 0, bi$gram,
+                          #                     #Implement POS tagger
+                          "the"
+                  )))
+}
--- shiny/nlprediction/server.R
+++ shiny/nlprediction/server.R
@@ -1,11 +1,11 @@
 library(shiny)
+source("predict.R")
 shinyServer(
   function(input, output){
-    model.tri <- readRDS("model_tri_sample_EN.RDS")
     fragment <- renderPrint({input$fragment})
     frag <- renderText({input$fragment})
     #strip punctuation from input
     output$pred <- renderPrint(paste({input$fragment}, 
-                                     model.tri[[1]][model.tri[[1]]$idx == input$fragment, "x"]))
+                                     stupidBackoff(input$fragment)))
   }
 )
