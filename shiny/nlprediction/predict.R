dfa3bbedb689224733d48e9e9d4c800cf09570af
jwillage <jwillage@gmail.com>
2016-01-07 15:38:52 -0500

    hash-based models to lower memory

--- shiny/nlprediction/predict.R
+++ shiny/nlprediction/predict.R
@@ -1,8 +1,17 @@
-bigram.model.samp <- readRDS("model_bi_sample_100k_EN_new.RDS")
-trigram.model.samp <- readRDS("model_tri_sample_100k_EN_new.RDS")
-quad.model.samp <- readRDS("model_quad_sample_100k_EN_new.RDS")
 library(openNLP)
-#library(NLP)
+library(NLP)
+
+#bigram.model.sing <- readRDS("model_bi_combined.RDS")
+#trigram.model.sing <- readRDS("model_tri_combined.RDS")
+#quadgram.model.sing <- readRDS("model_quad_combined.RDS")
+bigram.model.sing <- readRDS("model_bi_combined_hash.RDS")
+trigram.model.sing <- readRDS("model_tri_combined_hash.RDS")
+quadgram.model.sing <- readRDS("model_quad_combined_hash.RDS")
+pos <- readRDS("pos.RDS")
+
+sta <- Maxent_Sent_Token_Annotator()
+wta <- Maxent_Word_Token_Annotator()
+pta <- Maxent_POS_Tag_Annotator()
 
 cleanInput <- function(phrase){
   library(quanteda)
@@ -15,24 +24,25 @@ stupidBackoff <- function(phrase){
   len <- length(phrase)
   bi <- NULL; tri <- NULL; quad <- NULL;
   if (len >= 1) 
-    bi <- bigram.model.samp[bigram.model.samp$idx == 
-                              paste(phrase[len], collapse = " "), ]
+    bi <- bigram.model.sing[bigram.model.sing$idx == 
+                              hash(paste(phrase[len], collapse = " ")), ]
   if (len >= 2)
-    tri <- trigram.model.samp[trigram.model.samp$idx == 
-                                paste(phrase[(len - 1) : len], collapse = " "), ]
+    tri <- trigram.model.sing[trigram.model.sing$idx == 
+                                hash(paste(phrase[(len - 1) : len], collapse = " ")), ]
   if (len >= 3)
-    quad <- quad.model.samp[quad.model.samp$idx == 
-                              paste(phrase[(len - 2) : len], collapse = " "), ]
+    quad <- quadgram.model.sing[quadgram.model.sing$idx == 
+                              hash(paste(phrase[(len - 2) : len], collapse = " ")), ]
 
   ifelse (c(length(quad$gram) > 0, length(quad$gram) > 0), c(quad$gram, 4), 
           ifelse (c(length(tri$gram) > 0, length(tri$gram) > 0), c(tri$gram, 3), 
                   ifelse (c(length(bi$gram) > 0, length(bi$gram) > 0), c(bi$gram, 2),
-                          #                     #Implement POS tagger
-                          c("the", 1)
-                  )))
+                        #  c("the", 1) ))) #POS tagger
+                        ifelse(len > 0, c(
+                          pos[pos$pos == annotate(phrase[len], pta, annotate(phrase[len], 
+                           list(sta, wta)))[2]$features[[1]][[1]], "pred"], 1), c("", 1))) ))
 }
 
-stupidBackoff <- function(phrase, bi.model, tri.model, quad.model){
+stupidBackoffa <- function(phrase, bi.model, tri.model, quad.model){
   phrase <- strsplit(phrase, " ")[[1]]
   len <- length(phrase)
   bi <- NULL; tri <- NULL; quad <- NULL;
@@ -48,8 +58,6 @@ stupidBackoff <- function(phrase, bi.model, tri.model, quad.model){
                   ifelse (c(length(bi$gram) > 0, length(bi$gram) > 0), c(bi$gram, 2),
                           #                     #Implement POS tagger
                           c("the", 1)
-                        #  annotate(phrase[len], pos_tag_annotator, annotate(phrase[len], 
-                         # list(sent_token_annotator, word_token_annotator)))[2]$features[[1]][[1]]
                   )))
 }
 
--- shiny/nlprediction/server.R
+++ shiny/nlprediction/server.R
@@ -1,10 +1,11 @@
 library(shiny)
+library(hashr)
 source("predict.R")
+
 shinyServer(
   function(input, output){
     fragment <- renderPrint({input$fragment})
     frag <- renderText({input$fragment})
-    #strip punctuation from input and covert to lower
     output$pred <- renderPrint(paste({input$fragment}, 
                                      stupidBackoff(cleanInput(input$fragment))[1]))
   }
